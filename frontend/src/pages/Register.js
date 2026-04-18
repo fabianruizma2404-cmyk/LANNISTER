d4d49972b10fa1cceb0bbcc6e1126742b4b63ccf
fabianruizma2404-cmyk <fabianruizma2404@gmail.com>
2026-04-18 06:47:53 -0500

    Create Register.js

---
+++ frontend/src/pages/Register.js
@@ -0,0 +1,43 @@
+const CODIGO_SECRETO = "EXPORTIA2026";
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [codigo, setCodigo] = useState("");
+
+  const registrar = async () => {
+
+    if (codigo !== CODIGO_SECRETO) {
+      alert("Código inválido");
+      return;
+    }
+
+    const res = await fetch("https://lannister-production.up.railway.app/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ email, password })
+    });
+
+    const data = await res.json();
+
+    if (data.msg) {
+      alert("Usuario creado");
+    } else {
+      alert("Error");
+    }
+  };
+
+  return (
+    <div className="login-box">
+      <h2>Registro</h2>
+
+      <input placeholder="Email" onChange={e=>setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)} />
+      <input placeholder="Código de acceso" onChange={e=>setCodigo(e.target.value)} />
+
+      <button onClick={registrar}>Registrarse</button>
+    </div>
+  );
+}
